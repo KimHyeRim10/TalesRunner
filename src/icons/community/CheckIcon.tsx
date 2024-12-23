910c259373a09e64096a1bb7a1642eb73d5698bf
hyerim <hnn30011@gmail.com>
2024-12-24 02:09:04 +0900

    refactor: separate icons into reusable component

---
+++ src/icons/community/CheckIcon.tsx
@@ -0,0 +1,20 @@
+export default function CheckIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="stroke-gray-600"
+        d="M16.6666 5L7.49998 14.1667L3.33331 10"
+        stroke="#475467"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+}
---
+++ src/icons/community/ChevronDownIcon.tsx
@@ -0,0 +1,19 @@
+export default function ChevronDownIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 8L10 13L15 8"
+        stroke="#98A2B3"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+}
