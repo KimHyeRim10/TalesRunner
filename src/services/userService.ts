043c3b08f2634d2c1cd0b0a84656ee19e11a5970
hyerim <hnn30011@gmail.com>
2024-12-24 02:11:58 +0900

    feat: add user-related functions

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -9,7 +9,33 @@ export const getProfile = async (email: string): Promise<string> => {
 
     return response.data.profileURL; // 성공 시 데이터 반환
   } catch (error: any) {
-    console.error(error.response?.data?.error || error.message);
+    console.log(error.response?.data?.error || error.message);
+    throw new Error(error.response?.data?.error || error.message); // 에러를 상위에서 처리
+  }
+};
+
+export const getLevel = async (email: string): Promise<string> => {
+  try {
+    const response = await axios.post("/api/services/getUserLevel", {
+      email,
+    });
+
+    return response.data.levelURL; // 성공 시 데이터 반환
+  } catch (error: any) {
+    console.log(error.response?.data?.error || error.message);
+    throw new Error(error.response?.data?.error || error.message); // 에러를 상위에서 처리
+  }
+};
+
+export const getNicknameColor = async (email: string): Promise<string> => {
+  try {
+    const response = await axios.post("/api/services/getUserNicknameColor", {
+      email,
+    });
+
+    return response.data.nicknameColor; // 성공 시 데이터 반환
+  } catch (error: any) {
+    console.log(error.response?.data?.error || error.message);
     throw new Error(error.response?.data?.error || error.message); // 에러를 상위에서 처리
   }
 };
