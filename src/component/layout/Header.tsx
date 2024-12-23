cf08e67ca03ec6cd259d805bcc15433639aad3de
hyerim <hnn30011@gmail.com>
2024-12-24 02:15:48 +0900

    feat: add logout handler

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -1,6 +1,27 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useUser } from "@/context/UserContext";
+import { removeUser } from "@/utils/localStorage";
 
 export default function Header() {
+  const router = useRouter();
+  const { profileURL, user, setUser } = useUser();
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  const handleToggleDropdown = () => {
+    setToggleDropDown(!toggleDropDown);
+  };
+
+  const handleLogout = () => {
+    alert("로그아웃 되었습니다");
+    removeUser();
+    setUser(null); // 로그아웃 후 상태 업데이트
+    router.push("/");
+  };
+
   return (
     <div className="px-12 h-[48px] border-b border-gray-200 flex justify-between items-center">
       <div className="flex-center gap-2">
@@ -24,18 +45,79 @@ export default function Header() {
         </Link>
       </div>
 
-      <div className="flex-center gap-4">
-        <Link href={"/signup"}>
-          <button className=" text-[15px] font-[600] text-gray-600">
-            회원가입
-          </button>
-        </Link>
-        <Link href={"/login"}>
-          <button className="w-[84px] py-[6px] border border-[#D6BBFB] text-[15px] font-[600] text-[var(--primary-color)] rounded-full">
-            로그인
-          </button>
-        </Link>
-      </div>
+      {user ? (
+        <div>
+          <div className="relative flex items-center justify-end min-w-[160px] h-[36px]">
+            <img
+              className="w-[33px] h-[33px] rounded-full border border-[var(--border-color)] mr-[10px]"
+              src={profileURL || "/home/no-character.png"}
+              alt="profile"
+            />
+            <div className="flex-center">
+              <p className="text-[15px] text-[#475467] font-[600]">
+                {user?.nickname}
+              </p>
+              <img
+                className="w-[14px] h-[15px] m-[8px]"
+                src="/header/ico-chevron-down-14.svg"
+                alt="chevron-down"
+                onClick={handleToggleDropdown}
+              />
+            </div>
+          </div>
+          {toggleDropDown && (
+            <div className="absolute z-50 right-[45px] top-[50px] p-[12px] bg-white rounded-[8px] w-[200px] h-[229px]">
+              <div className="flex flex-col w-[176px] h-[80px] mb-[16px] bg-[#FAF6FF] rounded-[8px] p-[12px]">
+                <div>
+                  <p className="flex justify-between items-center mb-[10px]">
+                    <span className="text-[12px] text-[#344054] font-bold">
+                      보유캐시
+                    </span>
+                    <span className="flex-center  text-[#344054] font-bold w-[33px] h-[22px] rounded-md text-[12px] bg-white border border-[var(--boarder-color)]">
+                      충전
+                    </span>
+                  </p>
+                  <p className="text-right text-[#8544E2] text-[12px] font-bold">
+                    0
+                  </p>
+                </div>
+              </div>
+              <div className="pl-[12px] text-[12px] text-[#344054] font-bold">
+                <ul className="flex flex-col">
+                  <li className="pb-[12px]">회원정보</li>
+                  <li className="pb-[12px]">보안설정</li>
+                  <li className="flex items-center gap-1">
+                    <p>고객센터</p>
+                    <img src="/header/ico-link-new-14.svg" alt="link-new-" />
+                  </li>
+                </ul>
+              </div>
+
+              <div className="w-[176px] h-[1px] bg-[#EAECF0] mt-[12px]"></div>
+
+              <button
+                onClick={handleLogout}
+                className="text-[#344054] pl-[12px] text-[12px] font-bold"
+              >
+                로그아웃
+              </button>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex-center gap-4">
+          <Link href={"/signup"}>
+            <button className=" text-[15px] font-[600] text-gray-600">
+              회원가입
+            </button>
+          </Link>
+          <Link href={"/login"}>
+            <button className="w-[84px] py-[6px] border border-[#D6BBFB] text-[15px] font-[600] text-[var(--primary-color)] rounded-full">
+              로그인
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
