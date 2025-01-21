0a6613f0d14553938849010f014457caeee216bd
hyerim <hnn30011@gmail.com>
2025-01-21 21:57:48 +0900

    feat: Add environment variable for login API route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,6 +14,7 @@ import { useUser } from "@/context/UserContext";
 type InputRefs = Record<string, MutableRefObject<HTMLInputElement | null>>;
 
 export default function Login() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
   const { formData, handleChange, clearFormData } = useForm();
   const router = useRouter();
   const { refreshUserData } = useUser();
@@ -57,7 +58,7 @@ export default function Login() {
     }
 
     try {
-      const response = await axios.post("/api/auth/login", {
+      const response = await axios.post(`${apiUrl}/api/auth/login`, {
         email: formData.email,
         password: formData.userPass,
         captchaToken: failedAttempts >= 3 ? captchaToken : undefined, // CAPTCHA 토큰 전송
